b3fa6dc251143a7c1287cdab37f70a9200928637
moonchildxjay <drjw0613@yeonsung.ac.kr>
2022-05-20 10:05:53 +0900

    first

---
+++ app/src/main/java/kr/ac/yeonsung/minn/mobileproject_0520_1/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.yeonsung.minn.mobileproject_0520_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.time_date_picker);
+    }
+}
